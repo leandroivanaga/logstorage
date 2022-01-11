167b70dbfce1c45b5f58d405f68b10cca524728b
Leandro Ivanaga <leandro.ivanaga@gmail.com>
2022-01-11 09:31:01 -0300

    Create FileDto.php

---
+++ src/Dtos/FileDto.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Dtos;
+
+class FileDto
+{
+    public string $pid;
+    public string $disk;
+    public string $fileName;
+
+    public function getFileData(): array
+    {
+        return [
+            'pid' => $this->pid,
+            'disk' => $this->disk,
+            'file_name' => $this->fileName,
+        ];
+    }
+}
