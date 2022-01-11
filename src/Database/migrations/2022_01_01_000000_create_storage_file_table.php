f5645bb5eda29be6e6eed1b01645b467602fb9cf
Leandro Ivanaga <leandro.ivanaga@gmail.com>
2022-01-11 09:26:35 -0300

    Create 2022_01_01_000000_create_storage_file_table.php

---
+++ src/Database/migrations/2022_01_01_000000_create_storage_file_table.php
@@ -0,0 +1,27 @@
+<?php
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStorageFileTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('storage_file', function (Blueprint $table) {
+            $table->id();
+            $table->string('pid');
+            $table->string('disk');
+            $table->string('file_name');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('storage_file');
+    }
+}
