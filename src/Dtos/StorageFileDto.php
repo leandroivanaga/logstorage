eecee9572601e19706c98052c5f3f5946a8d5382
Leandro Ivanaga <leandro.ivanaga@gmail.com>
2022-01-11 09:49:09 -0300

    Update StorageFileDto.php

--- src/Dtos/StorageFileDto.php
+++ src/Dtos/StorageFileDto.php
@@ -5,8 +5,11 @@ namespace Logcomex\LogStorage\Dtos;
 class StorageFileDto
 {
     public string $pid;
-    public string $disk;
-    public string $fileName;
+    public string $fileIdentifier;
+    public string $content;
+    public string $extension;
+    public ?string $fileName;
+    public ?string $disk;
 
     public function getFileData(): array
     {
